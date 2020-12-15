90df4f1f55262d7307b3bfb227af724654ea7f35
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2020-12-15 11:09:32 +0530

    first commit with responsive home page

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+/*function externalLinks() {
+  for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++)
+  {
+      var b = c[a];
+      b.getAttribute('href') && b.hostname !== location.hostname &&(b.target = '_blank')
+  }
+}
+externalLinks();*/
+
+// Animate on scroll library initialization
+AOS.init({
+  duration:1500
+});
+
