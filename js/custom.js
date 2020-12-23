c140965fb91fce5f9c46df88bdb63e69dcb92998
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2020-12-23 15:20:18 +0530

    projects page added and responsive

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,37 @@
-/*function externalLinks() {
+function externalLinks() {
   for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++)
   {
       var b = c[a];
       b.getAttribute('href') && b.hostname !== location.hostname &&(b.target = '_blank')
   }
 }
-externalLinks();*/
-
-// Animate on scroll library initialization
+externalLinks();
 AOS.init({
   duration:1500
 });
+
+
+//owl carousel initialisation
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  dots:false,
+  autoplay:true,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+      },
+      600:{
+          items:1,
+      },
+      1000:{
+          items:1,
+      }
+  }
+})
+
+
+// Animate on scroll library initialization
+
 
