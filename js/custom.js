3a79a158b9493f29a539de9bf39feed7ada700fd
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2020-12-30 12:53:21 +0530

    new page added and more css changes

--- js/custom.js
+++ js/custom.js
@@ -6,10 +6,35 @@ function externalLinks() {
   }
 }
 externalLinks();
+
+// Animate on scroll library initialization
 AOS.init({
   duration:1500
 });
 
+/*window.onscroll = function(){
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.querySelector(".navigation").style.position = "fixed";
+    document.querySelector(".navigation").style.width = "100%";
+    document.querySelector(".navigation").style.background = "white";
+    document.querySelector(".navigation").style.zIndex = "100";
+    document.querySelector(".navigation").style.boxShadow = "3px 10px 20px #a7a7a7";
+    document.querySelector(".navigation").style.padding = "0.6rem";
+    
+    document.querySelector(".blm").style.display = "none";
+  } else {
+    document.querySelector(".navigation").style.position = "static";
+    document.querySelector(".navigation").style.width = "initial";
+    document.querySelector(".navigation").style.background = "initial";
+    document.querySelector(".navigation").style.zIndex = "initial";
+    document.querySelector(".navigation").style.boxShadow = "none";
+    document.querySelector(".blm").style.display = "flex";
+  }
+}*/
 
 //owl carousel initialisation
 $('.owl-carousel').owlCarousel({
@@ -32,6 +57,7 @@ $('.owl-carousel').owlCarousel({
 })
 
 
-// Animate on scroll library initialization
+
+
 
 
