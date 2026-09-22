47836dffa760c371a103819b62455a5cc91bb24f
pratitb <pratit.bangdiwala7@gmail.com>
2026-09-22 22:46:40 +0530

    feat: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,11 @@ interface HeaderProps {
 
 const Header = ({ name, role }: HeaderProps) => {
     return (
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-2 items-center mb-8">
             <img src="./profile-3.png" alt="" className="w-12 rounded-full" />
             <div>
                 <p className="headText">{name}</p>
-                <p className="capitalize">{role}</p>
+                <p className="subHead mt-1">{role}</p>
             </div>
         </div>
     )
