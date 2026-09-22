f0a8dc2045f8abf3c209f752826657c39426ebb3
pratitb <pratit.bangdiwala7@gmail.com>
2026-09-22 22:47:41 +0530

    feat: work, app

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -13,14 +13,14 @@ interface WorkProps {
 const WorkCard = ({ imageSrc, title, desc, projectLink }: WorkProps) => {
     // getStatus, getName,
     return (
-        <div className='bg-bgCard rounded-lg shadow-lg p-6'>
+        <div className='bg-bgCard shadow-lg rounded-lg p-4'>
             <div className='relative'>
                 <img src={imageSrc} alt="" className='rounded-lg' />
             </div>
-            <div className=''>
-                <p className='font-semibold text-lg capitalize mt-2'>{title}</p>
-                <p>{desc}</p>
-                <div className='flex items-center gap-2 underline uppercase text-xs font-medium tracking-wider mt-4'>
+            <div className='p-2'>
+                <p className='font-bold text-lg capitalize mt-2'>{title}</p>
+                <p className='text-subtle text-sm'>{desc}</p>
+                <div className='flex items-center gap-1 uppercase text-xs font-medium tracking-wide text-secondary mt-4'>
                     <Link to={projectLink}>view project</Link>
                     <LuArrowUpRight />
                 </div>
